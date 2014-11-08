ec2dae4897624db9d1eadb6a7e5c67f651968fc8
paullllllll <schlenkerpc@hotmail.com>
2014-11-08 01:13:12 +0000

    here

--- build/testgame/serverside.js
+++ build/testgame/serverside.js
@@ -1,12 +1,19 @@
 var http = require('http');
 var dgram = require('dgram');
 var fs = require('fs');
-
+var first = true;
 var server = http.createServer( function(req, res){
-       
        res.writeHead(200, 'good');
-       fs.createReadStream('./testgame.html').pipe(res);
-       
+       if(first){
+              
+              fs.createReadStream('./testgame.html').pipe(res);
+              first=false;
+       }
+       else{
+              
+              fs.createReadStream(req.url).pipe(res);
+              
+       }
        
 });
 
