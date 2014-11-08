54b0bc9586fbf17bd960ada13ac0a8f084db2185
paullllllll <schlenkerpc@hotmail.com>
2014-11-08 02:17:56 +0000

    ugh

--- build/testgame/serverside.js
+++ build/testgame/serverside.js
@@ -6,18 +6,11 @@ var url = require("url");
 var server = http.createServer( function(req, res){
        res.writeHead(200, 'good');
        
-       var parsedUrl = url.parse(req.url, true);
+      
        
-       if(!parsedUrl.pathname){
-              
-              fs.createReadStream('./testgame.html').pipe(res);
-       }
-       else{
-              
-              fs.createReadStream('.'+parsedUrl.pathname).pipe(res);
-              
-       }
-       
+       console.log(req.url);
+
+       fs.createReadStream('./testgame.html').pipe(res);
        
 });
 
