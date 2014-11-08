39f28ce1154f8f9f5d0e8ae2220035ada8fec607
paullllllll <schlenkerpc@hotmail.com>
2014-11-08 01:27:58 +0000

    phewa

--- build/testgame/serverside.js
+++ build/testgame/serverside.js
@@ -6,16 +6,13 @@ var url = require("url");
 var server = http.createServer( function(req, res){
        res.writeHead(200, 'good');
        
-       console.log(req.url.toString()+'work');
               
        fs.createReadStream('./testgame.html').pipe(res);
        
-       fs.createReadStream(req.url).pipe(res);
               
        
        
 });
 
-console.log('hi');
 
 server.listen(8080);
