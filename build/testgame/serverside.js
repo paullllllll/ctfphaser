8a87816b132cf902b9d04610f0e53139115985df
paullllllll <schlenkerpc@hotmail.com>
2014-11-09 02:20:55 +0000

    ugh

--- build/testgame/serverside.js
+++ build/testgame/serverside.js
@@ -2,6 +2,8 @@ var http = require('http');
 var dgram = require('dgram');
 var fs = require('fs');
 var url = require("url");
+var ipware = require('ipware');
+
 
 var server = http.createServer( function(req, res){
        res.writeHead(200, 'good');
@@ -20,6 +22,8 @@ var server = http.createServer( function(req, res){
        
        
 });
+
+var socket = dgram.createSocket('udp4');
 
 
 server.listen(8080);
